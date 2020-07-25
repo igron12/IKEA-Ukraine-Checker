b1a05bb97d1bf5359827e8d6a4126b247514ccd8
@sived <igron12@me.com>
2020-07-25 16:53:20 +0300

    Changed structure. Added new class Item

--- src/CountriesChecker.java
+++ src/CountriesChecker.java
@@ -1,55 +1,17 @@
 import countries.*;
-import org.jsoup.HttpStatusException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 public class CountriesChecker {
     ArrayList<Country> list = new ArrayList<>(Arrays.asList(new Ukraine(), new Poland(), new USA(),
             new UK(), new Slovakia(), new Germany(), new Croatia(), new Sweden(), new Canada()));
-    TreeMap<Double, String> map;
-    ArrayList<String> notAvailable;
-
-    String baseCreator = "https://www.ikea.com/";
 
     public void check(String name) {
-        map = new TreeMap<>();
-        notAvailable = new ArrayList<>();
+        Item item = new Item();
 
-        list.parallelStream().forEach(item->
-        {
+        list.parallelStream().forEach(country->
+                item.check(country, name));
 
-            try {
-                Document doc = Jsoup.connect(baseCreator + item.id + "/" + item.lang + "/p/-" + name).execute().parse();
-
-                //Price
-                Price local = new Price(doc, item);
-                map.put(local.get(), item.countryName + local.toString());
-
-                //Rating
-                map.put(local.get(), map.get(local.price) + new Rating(doc));
-
-            } catch (HttpStatusException e) {
-                notAvailable.add(item.countryName);
-            } catch (NumberFormatException e) {
-                System.out.println("NumberFormatException " + e.getLocalizedMessage());
-            } catch (Exception ignored) {}
-
-        });
-
-        writeResult();
-    }
-
-    public void writeResult() {
-        for (Double price : map.keySet()) {
-            System.out.println((int) Math.round(price) + " грн in " + map.get(price));
-        }
-
-        if (notAvailable.size() > 0) System.out.println("Product is not available in " + notAvailable.stream().map(Object::toString).collect(Collectors.joining(", ")));
-
+        item.writeResult();
     }
 }
